41012ee6d513c1958cfb3d29d33287bc61eff912
Arif Swozon <swazan.arif@gmail.com>
2019-07-19 21:39:51 +0600

    Some changes

--- js/index.js
+++ js/index.js
@@ -21,10 +21,10 @@ $( document ).ready(function() {
         myTimer = setInterval(function () {
             count++;
             if (count >= myObject.wordsArray.length) {
-                $('#myModal').modal('toggle');
+                $('#myModal').modal('hide');
                 clearInterval(myTimer);
             }
-            // $(".modal-body").html(myObject.wordsArray[count])
+            // $("#modal-body-reading").html(myObject.wordsArray[count])
             $("#modal-body-reading").fadeOut(counter/2, function () {
                 $(this).text(myObject.wordsArray[count]).fadeIn(counter/2);
             });
@@ -34,15 +34,35 @@ $( document ).ready(function() {
 
     $(document).on("click","#settingSaveButton", function (event) {
         if ($('#wordPerMinute').val() == '') {
-            $('.error').css('display', 'block');
+            $('.errorForWPM').css('display', 'block');
             return false;
         }
         myObject.wordPerMinute = $('#wordPerMinute').val();
         $('#settingModal').modal('toggle');
     });
 
+    $(document).on("click","#addNewSaveButton", function (event) {
+        if ($('#addNew').val() == '') {
+            $('.errorForAddNew').css('display', 'block');
+            return false;
+        }
+
+        $("#articleContainerRow").append("<div class='col-sm-12 col-md-6 col-lg-6'>"+
+            "<div class='panel panel-default'>"+
+                "<div class='panel-body article' id='article2' data-toggle='modal' data-target='#myModal'>"+
+                    $('#addNew').val()+
+                "</div>"+
+            "</div>"+
+        "</div>");
+        $('#addNewModal').modal('toggle');
+    });
+
     $(document).on("keyup","#wordPerMinute", function (event) {
-        $('.error').css('display', 'none');        
+        $('.errorForWPM').css('display', 'none');        
+    });
+
+    $(document).on("keyup","#addNew", function (event) {
+        $('.errorForAddNew').css('display', 'none');        
     });
 
     $('#myModal').on('hide.bs.modal', function (e) {
